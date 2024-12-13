91a7c8c2baf15779ce18433c5cee4af0e6af7f4f
viniciusnascimento-dev <viniciusdonascimento.dev@gmail.com>
2024-12-12 23:43:02 -0300

    Correção de variável e msg final

--- index.js
+++ index.js
@@ -1,25 +1,25 @@
 let nome = "Avatar";
-let xp = 8100;
-let msg;
+let xp = 9000;
+let nivel;
 
 if (xp > 0 && xp <= 1000) {
-   msg = "Ferro";
+   nivel = "Ferro";
 } else if (xp >= 1001 && xp <= 2000) {
-    msg = "Bronze"
+    nivel = "Bronze"
 } else if (xp >= 2001 && xp <= 5000) {
-    msg = "Prata"
+    nivel = "Prata"
 } else if (xp >= 5001 && xp <= 7000) {
-    msg = "Ouro"
+    nivel = "Ouro"
 } else if (xp >= 7001 && xp <= 8000) {
-    msg = "Platina"
+    nivel = "Platina"
 } else if (xp >= 8001 && xp <= 9000) {
-    msg = "Ascendente"
+    nivel = "Ascendente"
 } else if (xp >= 9001 && xp <= 10000) {
-    msg = "Imortal"
+    nivel = "Imortal"
 } else if (xp >= 10001) {
-    msg = "Radiante"   
+    nivel = "Radiante"   
 } else {
-    msg = "Jogador(a) sem XP"
+    nivel = "Jogador(a) sem XP"
 }
 
-console.log(msg);
+console.log(`O herói de nome ${nome} está no nível de ${nivel}`);
